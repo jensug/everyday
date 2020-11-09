86c97409fd4f40798ff88e562e8959578523708f
jensug <jensug@ifi.uio.no>
2020-11-09 13:42:34 +0100

    hello

---
+++ script/javascript.js
@@ -0,0 +1,24 @@
+
+
+document.querySelector('.nav-select-wrapper').addEventListener('click', function() {
+    this.querySelector('.nav-select').classList.toggle('open');
+})
+
+
+for (const option of document.querySelectorAll(".nav-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.nav-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.nav-select').querySelector('.nav-select_trigger span').textContent = this.textContent;
+        }
+    })
+}
+
+
+window.addEventListener('click', function(e) {
+    const select = document.querySelector('.nav-select')
+    if (!select.contains(e.target)) {
+        select.classList.remove('open');
+    }
+});
